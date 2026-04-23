8043128b526b26d8f64abcd991be959c644734a0
LeboMotaungTNG <LeboMotaungTNG@users.noreply.github.com>
2026-04-23 09:22:54 +0200

    Revert "Update ManagerDashboard.tsx"
    This reverts commit 2ea788ac47c70fbedc11b1b1fcfcf6f9833d4b74.

--- apps/web/src/apps/Manager/ManagerDashboard.tsx
+++ apps/web/src/apps/Manager/ManagerDashboard.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SharedLayout from "./SharedLayout";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://employee-evaluation-kago-e63baae4d822.herokuapp.com/api/v1";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1";
 const token = () => localStorage.getItem("token") || "";
 
 function getMonthYear(d: Date) {
